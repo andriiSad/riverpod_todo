35eefa2bc20314fd672768a8fd5fb94e5cca9ba7
Andrii Sadovyi <waterpolomen2@gmail.com>
2023-06-23 13:55:58 +0200

    created project structure

---
+++ lib/common/widgets/app_style.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextStyle appStyle(
+  double fontSize,
+  Color color,
+  FontWeight fontWeight,
+) =>
+    GoogleFonts.poppins(
+      fontSize: fontSize.sp,
+      color: color,
+      fontWeight: fontWeight,
+    );
